89f3b3e916540b8e74c20b8a39b6f656587e4ea4
gines <gines@GINESH-PC>
2024-06-11 11:13:43 +0530

    design asstes

---
+++ Onyx_POS/wwwroot/js/page/login.js
@@ -0,0 +1,11 @@
+﻿function showHidePasswordAsterisk(id, curEl) {
+    var x = document.getElementById(id);
+    if (x.type === "password") {
+        x.type = "text";
+        $(curEl).parent().find(".eye-icon").removeClass("fa-eye").addClass("fa-eye-slash");
+    }
+    else {
+        x.type = "password";
+        $(curEl).parent().find(".eye-icon").removeClass("fa-eye-slash").addClass("fa-eye");
+    }
+}
